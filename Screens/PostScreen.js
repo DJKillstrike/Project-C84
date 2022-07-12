ec14806fd3806184c1482cfb5d1b90e4365aa5d6
DJKillstrike <74912481+DJKillstrike@users.noreply.github.com>
2022-07-12 19:49:40 +0530

    Add files via upload
    Works in IOS and web, doesn't work on android....

---
+++ Screens/PostScreen.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { RFValue } from 'react-native-responsive-fontsize';
+import AppLoading from 'expo-app-loading';
+
+export default class PostScreen extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <View>
+            <View style={styles.authorImageContainer}>
+              <Image
+                source={require('../assets/profile_img.png')}
+                style={styles.profileImage}>
+              </Image>
+            </View>
+            <View style={{paddingTop:20,paddingBottom:10}}>
+              <Text style={styles.authorTitleText}>{this.props.route.params.post.author}</Text>
+            </View>
+            <Image style={styles.storyImage} source={require('../assets/post.jpeg')}></Image>
+            <View style={{padding:20}}>
+              <Text style={styles.captionText}>{this.props.route.params.post.caption}</Text>
+            </View>
+            <View style={styles.actionContainer}>
+              <View style={styles.likeButton}>
+                <Ionicons name={'heart'} size={RFValue(30)} color={'white'} />
+                <Text style={styles.likeText}>13K</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#101010',
+  },
+  cardContainer: {
+    margin: RFValue(10),
+    backgroundColor: '#181818',
+    borderRadius: RFValue(20),
+  },
+  storyImage: {
+    resizeMode: 'contain',
+    width: '95%',
+    alignSelf: 'center',
+    borderRadius: RFValue(20),
+    height: RFValue(250),
+  },
+  authorTitleText: {
+    fontSize: RFValue(20),
+    alignSelf: 'center',
+    color: 'white',
+  },
+  actionContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: RFValue(10),
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#eb3948',
+    borderRadius: RFValue(30),
+  },
+  likeText: {
+    color: 'white',
+    fontFamily: 'Bubblegum-Sans',
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5),
+  },
+  authorImageContainer: {
+    paddingTop:10,
+  },
+  profileImage: {
+    resizeMode: 'contain',
+    width: '20%',
+    alignSelf: 'center',
+    height: RFValue(20),
+  },
+  captionText: {
+    fontSize: RFValue(18),
+    alignSelf: 'left',
+    color: 'white',
+  },
+  
+});
---
+++ navigation/TabNavigator.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Feed from '../Screens/Feed';
+import CreatePost from '../Screens/CreatePost';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Feed') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'CreateStory') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          }
+
+          return (
+            <Ionicons
+              name={iconName}
+              size={RFValue(25)}
+              color={color}
+              style={styles.icons}
+            />
+          );
+        },
+      })}
+      activeColor = {"orange"}
+      inactiveColor = {"gray"}
+      >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="CreateStory" component={CreatePost} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: '#181818',
+    height: '10%',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30),
+  },
+});
+
+export default BottomTabNavigator;
